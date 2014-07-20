e841209932ef321fb97e548a363db5dabf38ba8b
Anna Valkov <anna.a.valkov@gmail.com>
2014-07-20 13:02:49 -0700

    created a new prototype to calculate the perimeter of the triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
+
+var Triangle = {
+	triangleCalculator: function(){
+		  if ((((this.sideOne + this.sideTwo) < this.sideThree) || ((this.sideTwo + this.sideThree) < this.sideOne) || ((this.sideThree + this.sideTwo) < this.sideOne))) {
+              return "NOT A TRIANGLE";
+            } else if (this.sideOne === this.sideTwo && this.sideOne === this.sideThree) {
+              return "EQUILATERAL";
+            } else if (this.sideOne === this.sideThree || this.sideTwo === this.sideOne || this.sideThree === this.sideTwo) {
+              return "ISOCELES";
+            } else if ((this.sideOne !== this.sideTwo && this.sideOne !== this.sideThree) && (this.sideTwo !== this.sideThree)) {
+              return "SCALENE";
+            } else {
+              return false;
+            }
+	}
+};
+
+var TrianglePerimeter ={
+	trianglePerimeterCalc: function(){
+		return this.sideOne + this.sideTwo + this.sideThree 
+	}
+};
+
 $(document).ready(function(){
   $("form#triangle").submit(function(event){
- 
-  	var Triangle = {
-  		triangleCalculator: function(){
-  			  if ((((this.sideOne + this.sideTwo) < this.sideThree) || ((this.sideTwo + this.sideThree) < this.sideOne) || ((this.sideThree + this.sideTwo) < this.sideOne))) {
-                  return "NOT A TRIANGLE";
-                } else if (this.sideOne === this.sideTwo && this.sideOne === this.sideThree) {
-                  return "EQUILATERAL";
-                } else if (this.sideOne === this.sideThree || this.sideTwo === this.sideOne || this.sideThree === this.sideTwo) {
-                  return "ISOCELES";
-                } else if ((this.sideOne !== this.sideTwo && this.sideOne !== this.sideThree) && (this.sideTwo !== this.sideThree)) {
-                  return "SCALENE";
-                } else {
-                  return false;
-                }
-  		}
-  	};
-
+ 	event.preventDefault();
+  	
   	var sidesEntered = Object.create(Triangle);
   	sidesEntered.sideOne = parseInt($('input#side1').val());
   	sidesEntered.sideTwo = parseInt($('input#side2').val());
@@ -34,8 +42,12 @@ $(document).ready(function(){
         return false;
       }
 
+      $('input#side1').val("");
+      $('input#side2').val("");
+      $('input#side3').val("");
+      
 
- 	event.preventDefault();
+
   });
 
  });
